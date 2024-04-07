d5c7779a8b2d2741bae8b556db7add5518c9b8ae
MaazBinAamir <maamir.bese23seecs@seecs.edu.pk>
2024-04-07 15:48:39 +0500

    updatev1

--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -21,37 +21,23 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 
-public class LoginAndRegister extends AppCompatActivity {
+public class Login extends AppCompatActivity {
     private FirebaseAuth mAuth;
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-            finish();
-            Toast.makeText(LoginAndRegister.this, "Already Signed in.",
-                    Toast.LENGTH_SHORT).show();
-        }
-    }
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login_and_register);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
         mAuth = FirebaseAuth.getInstance();
-        TextView loginTxt = findViewById(R.id.txtMain);
-        loginTxt.setText(R.string.login_text);
     }
 
     public void onLoginClick(View view){
@@ -76,49 +62,15 @@ public class LoginAndRegister extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             FirebaseUser user = mAuth.getCurrentUser();
-                            Toast.makeText(LoginAndRegister.this, "Signed in Successfully.",
+                            Toast.makeText(Login.this, "Signed in Successfully.",
                                     Toast.LENGTH_SHORT).show();
                             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
                             startActivity(intent);
                             finish();
                         } else {
                             // If sign in fails, display a message to the user.
-                            Toast.makeText(LoginAndRegister.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-
-    }
-
-    public void onSignUpClick(View view){
-        EditText editUserEmailAddress = findViewById(R.id.editTextUserEmailAddress);
-        EditText editPassword = findViewById(R.id.editTextUserPassword);
-        TextView credentialsValidator = findViewById(R.id.credentialsValidator);
-        String email = String.valueOf(editUserEmailAddress.getText());
-        String password = String.valueOf(editPassword.getText());
-
-        if (TextUtils.isEmpty(email)){
-            credentialsValidator.setTextColor(Color.RED);
-            credentialsValidator.setText("Enter an email");
-        }
-        if (TextUtils.isEmpty(password)){
-            credentialsValidator.setTextColor(Color.RED);
-            credentialsValidator.setText("Enter a password");
-        }
-
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Toast.makeText(LoginAndRegister.this, "Signed Up Successfully.",
-                                    Toast.LENGTH_SHORT).show();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Toast.makeText(LoginAndRegister.this, "Authentication failed.",
+                            Toast.makeText(Login.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         }
                     }
