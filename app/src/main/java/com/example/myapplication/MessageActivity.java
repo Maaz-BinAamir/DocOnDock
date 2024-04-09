28c3bb9ea79c9caf92b576aabb57298077df4f23
MaazBinAamir <maamir.bese23seecs@seecs.edu.pk>
2024-04-09 15:29:07 +0500

    Add your stuff to it

--- app/src/main/java/com/example/myapplication/MessageActivity.java
+++ app/src/main/java/com/example/myapplication/MessageActivity.java
@@ -1,14 +1,12 @@
 package com.example.myapplication;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.widget.Toolbar;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -17,7 +15,6 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.bumptech.glide.Glide;
 import com.example.myapplication.Model.User;
-import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -26,34 +23,49 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.HashMap;
-
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MessageActivity extends AppCompatActivity {
 
     CircleImageView profile_image;
     TextView username;
 
+    Intent intent;
+
+
     FirebaseUser firebaseUser;
     DatabaseReference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_message);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         profile_image = findViewById(R.id.profile_pic);
         username = findViewById(R.id.username);
-        TextView welcomeText = findViewById(R.id.txtMain);
+
+        intent = getIntent();
+        String userid = intent.getStringExtra("userid");
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+        reference = FirebaseDatabase.getInstance().getReference("Users").child("userid");
 
         reference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -64,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
                     profile_image.setImageResource(R.mipmap.ic_launcher);
                 }
                 else {
-                    Glide.with(MainActivity.this).load(user.getImageURL()).into(profile_image);
+                    Glide.with(MessageActivity.this).load(user.getImageURL()).into(profile_image);
                 }
             }
 
@@ -73,22 +85,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        welcomeText.setText("WELCOME USER!");
-
     }
-
-    public void onLogOutClick(View view){
-        FirebaseAuth.getInstance().signOut();
-
-        Intent intent = new Intent(getApplicationContext(), Login.class);
-        startActivity(intent);
-        finish();
-    }
-
-    public void onContinueClick(View view){
-        Intent intent = new Intent(getApplicationContext(), MessageActivity.class);
-        startActivity(intent);
-        finish();
-    }
-
 }
